01c775b8858d0650e96fc0d22682597ce8f09cb0
Maxcoh <blb98@drexel.edu>
2018-03-06 18:37:16 -0500

    Initial commit of iPhone Storyboard

---
+++ EmergencyManagementApplication/EmergencyManagementApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmergencyManagementApplication
+//
+//  Created by Brendan Barnes on 3/6/18.
+//  Copyright © 2018 GloucesterCountyOEM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
